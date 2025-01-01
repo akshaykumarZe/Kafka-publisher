b81c5654b7b3e5c92a93350f39dc8ce54f7400c5
akshaykumarZe <akshaykumarZe@example.com>
2025-01-01 12:24:47 +0000

    new commit

--- index.js
+++ index.js
@@ -1,48 +1,26 @@
-const { Kafka } = require('kafkajs');
-import dotenv from "dotenv";
-dotenv.config();
+import { Kafka } from "kafkajs";
+ 
 const kafka = new Kafka({
-    clientId: 'nodejs-publisher',
-    brokers: [process.env.KAFKA_BROKER || "my-cluster-kafka-bootstrap.kafka:9092"],
-    sasl: {
-        mechanism: 'scram-sha-512',
-        username: process.env.KAFKA_SASL_USERNAME, // SASL username
-        password: process.env.KAFKA_SASL_PASSWORD, // SASL password
-    },
+  clientId: "my-app",
+  brokers: ["my-cluster-kafka-bootstrap.kafka:9092"],
+  sasl: {
+    mechanism: "scram-sha-512",
+    username: "my-connect-user",
+    password: "eWKhGtJJ16Fo9svPInU8Osw99zEZ44wt",
+  },
 });
-
+ 
 const producer = kafka.producer();
-const topic = process.env.KAFKA_TOPIC;
-const text = process.env.PUBLISH_TEXT;
-const number = process.env.PUBLISH_NUMBER;
-
-
-
-// Function to publish a message to Kafka
-const publishMessage = async () => {
-    try {
-        await producer.connect();
-        console.log(`Connected to Kafka broker`);
-
-        // Publish message every 5 seconds
-        setInterval(async () => {
-            const message = {
-                text: text,
-                number: Number(number)
-            };
-
-            await producer.send({
-                topic: topic,
-                messages: [{ value: JSON.stringify(message) }],
-            });
-
-            console.log(`Published message: ${JSON.stringify(message)} to topic: ${topic}`);
-        }, 5000);
-
-    } catch (error) {
-        console.error('Error publishing message:', error);
-    }
-};
-
-// Start publishing
-publishMessage();
+ 
+const data = new Array(1).fill({
+  value: "jhdcdvfdhvjfhbvfdbjhvdfbhvjhbfd",
+});
+ 
+await producer.connect();
+ 
+await producer.send({
+  topic: "testak",
+  messages: data,
+});
+ 
+await producer.disconnect();
