8dbc9573d70cf0c9595f87db64d6376d1410b92a
akshaykumarZe <akshaykumarZe@example.com>
2025-01-01 09:18:24 +0000

    sasl

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const { Kafka } = require('kafkajs');
 require('dotenv').config();
 
-// Kafka configuration from environment variables
 const kafka = new Kafka({
     clientId: 'nodejs-publisher',
-    brokers: [process.env.KAFKA_BROKER || "my-cluster-kafka-bootstrap.kafka:9092"]
+    brokers: [process.env.KAFKA_BROKER || "my-cluster-kafka-bootstrap.kafka:9092"],
+    ssl: process.env.KAFKA_SSL === 'true', // Enable SSL if required
+    sasl: {
+        mechanism: 'scram-sha-512',
+        username: process.env.KAFKA_SASL_USERNAME, // SASL username
+        password: process.env.KAFKA_SASL_PASSWORD, // SASL password
+    },
 });
 
 const producer = kafka.producer();
 const topic = process.env.KAFKA_TOPIC;
 const text = process.env.PUBLISH_TEXT;
 const number = process.env.PUBLISH_NUMBER;
+
+
 
 // Function to publish a message to Kafka
 const publishMessage = async () => {
